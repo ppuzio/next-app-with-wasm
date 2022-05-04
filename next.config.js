c9ab50ca337f1743039648f0a2fa13213e8d98f6
Paweł Puzio <pawel.puzio@twoupdigital.com>
2022-05-04 14:15:09 +0200

    update 2022

--- next.config.js
+++ next.config.js
@@ -13,18 +13,36 @@ module.exports = {
   future: {
     webpack5: true,
   },
-  webpack: (config) => {
+  webpack: (config, {isServer, dev}) => {
     // Ensures that web workers can import scripts.
     config.output.publicPath = '/_next/';
 
-    config.experiments = { asyncWebAssembly: true, syncWebAssembly: true };
+    config.experiments = { asyncWebAssembly: true, layers: true, syncWebAssembly: true };
 
-    config.plugins.push(
-      new WasmPackPlugin({
-        crateDirectory: resolve('./'),
-      })
-    );
+    if (!dev && isServer) {
+      config.output.webassemblyModuleFilename = "chunks/[id].wasm";
+      config.plugins.push(new WasmChunksFixPlugin());
+    }
 
     return config;
   },
 };
+
+class WasmChunksFixPlugin {
+  apply(compiler) {
+    compiler.hooks.thisCompilation.tap("WasmChunksFixPlugin", (compilation) => {
+      compilation.hooks.processAssets.tap(
+        { name: "WasmChunksFixPlugin" },
+        (assets) =>
+          Object.entries(assets).forEach(([pathname, source]) => {
+            if (!pathname.match(/\.wasm$/)) return;
+            compilation.deleteAsset(pathname);
+
+            const name = pathname.split("/")[1];
+            const info = compilation.assetsInfo.get(pathname);
+            compilation.emitAsset(name, source, info);
+          })
+      );
+    });
+  }
+}
